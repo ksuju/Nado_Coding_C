5e6b9276221a98e9b9c3e0e0fb8d37579ac9238f
ksuju <139477852+ksuju@users.noreply.github.com>
2024-11-28 23:39:30 +0900

    4일차

---
+++ ch3/loop.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// C언어 반복문
+	// for문
+	for (int i = 1; i <= 10; i++) {
+		printf("Hello %d\n", i);
+	}
+
+	int i = 1;
+
+	// while문
+	while (i <= 10) {
+		printf("while %d\n", i);
+		i++;
+	}
+
+	// do-while문
+	int j = 1;
+
+	do {
+		printf("do-while %d\n", j++);
+	} while (j <= 10);
+
+	return 0;
+}
