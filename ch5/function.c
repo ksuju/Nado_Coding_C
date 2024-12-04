5b9cdf8f259f2bf6305ed560d7fcd3ff30dd8248
ksuju <kyd547@naver.com>
2024-12-04 19:58:57 +0900

    함수

---
+++ ch5/function.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void p(int num);
+int add(int num1, int num2);
+
+int main(void) {
+	int num = 2;
+	p(num);
+
+	num += 3;
+	p(num);
+
+	num -= 1;
+	p(num);
+
+	num *= 3;
+	p(num);
+
+	num /= 6;
+	p(num);
+
+	return 0;
+}
+
+
+void p(int num) {
+	printf("num은 %d입니다.\n", num);
+}
+
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+
